83c43673043d9c50c4d8a4bc7ea46b14a2f3e764
DiverseBloom780 <98516443+DiverseBloom780@users.noreply.github.com>
2026-01-07 01:31:48 -0600

    Add GameWheelUI component as UserControl

---
+++ Arcadia.UI/GameWheelUI.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Windows;
+using System.Windows.Controls;
+namespace Arcadia.UI
+{
+    public class GameWheelUI : UserControl
+    {
+        // Code for the GameWheelUI component
+    }
+}
