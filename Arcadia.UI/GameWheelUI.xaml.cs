123b11ee90699e3f1dfaaa99554f0888238648dd
DiverseBloom780 <98516443+DiverseBloom780@users.noreply.github.com>
2026-01-07 01:33:06 -0600

    Create GameWheelUI.xaml.cs with initial setup
    Added code-behind for the GameWheelUI component.

---
+++ Arcadia.UI/GameWheelUI.xaml.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Windows;
+using System.Windows.Controls;
+namespace Arcadia.UI
+{
+    public partial class GameWheelUI : UserControl
+    {
+        // Code-behind for the GameWheelUI component
+    }
+}
