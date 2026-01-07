4d19c324ff20b95251fee496ffa229d2ccac5555
DiverseBloom780 <98516443+DiverseBloom780@users.noreply.github.com>
2026-01-07 01:41:24 -0600

    Add PCLauncherIntegration class for game management

---
+++ Arcadia.UI/PCLauncherIntegration.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Windows;
+using System.Windows.Controls;
+namespace Arcadia.UI
+{
+    public class PCLauncherIntegration
+    {
+        public void LaunchGame(string gameId)
+        {
+            // Launch the game using the PC launcher
+        }
+
+        public void GetInstalledGames()
+        {
+            // Get a list of installed games
+        }
+    }
+}
