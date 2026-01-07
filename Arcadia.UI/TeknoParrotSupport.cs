5bb074af13a184a92d6de0f3ea9008e9a53c8c27
DiverseBloom780 <98516443+DiverseBloom780@users.noreply.github.com>
2026-01-07 01:46:16 -0600

    Add TeknoParrotSupport class for game launching

---
+++ Arcadia.UI/TeknoParrotSupport.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Windows;
+using System.Windows.Controls;
+namespace Arcadia.UI
+{
+    public class TeknoParrotSupport
+    {
+        public void LaunchTeknoParrot(string romPath)
+        {
+            // Launch TeknoParrot with the specified ROM
+        }
+
+        public void GetAvailableTeknoParrotGames()
+        {
+            // Get a list of available TeknoParrot games
+        }
+    }
+}
