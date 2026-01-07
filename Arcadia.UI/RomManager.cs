a63cad4c4a063fa556df5532e73d0fd558540192
DiverseBloom780 <98516443+DiverseBloom780@users.noreply.github.com>
2026-01-07 01:45:33 -0600

    Add RomManager class for managing ROMs

---
+++ Arcadia.UI/RomManager.cs
@@ -0,0 +1,17 @@
+using System;
+using System.IO;
+namespace Arcadia.UI
+{
+    public class RomManager
+    {
+        public void AddRom(string romPath)
+        {
+            // Add the ROM to the ROM manager
+        }
+
+        public void RemoveRom(string romPath)
+        {
+            // Remove the ROM from the ROM manager
+        }
+    }
+}
