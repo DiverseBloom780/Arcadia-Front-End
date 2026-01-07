7f74c993adefe2f0a84d2a1b97fff5451e0b7b6f
DiverseBloom780 <98516443+DiverseBloom780@users.noreply.github.com>
2026-01-07 01:42:11 -0600

    Add SteamLauncher class for launching games

---
+++ Arcadia.UI/SteamLauncher.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Diagnostics;
+namespace Arcadia.UI
+{
+    public class SteamLauncher
+    {
+        public void LaunchGame(string gameId)
+        {
+            // Launch the game using Steam
+        }
+    }
+}
